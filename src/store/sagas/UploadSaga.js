2e66b0216686c36bd1ddf7e6b0189e58d61fe0a2
Selvakannan <selvakannan.sp@gmail.com>
2023-11-16 13:19:35 +0530

    APIKit

--- src/store/sagas/UploadSaga.js
+++ src/store/sagas/UploadSaga.js
@@ -1,52 +1,53 @@
-import {put, takeLatest, call} from 'redux-saga/effects';
-import axios from 'axios';
-import {
-  UPLOAD_VIDEO_REQUEST,
-  uploadVideoSuccess,
-  uploadVideoFailure,
-  setUploadProgress,
-} from '../actions/UploadActions';
-import store from '../Store';
-function* uploadVideo(action) {
-  try {
-    const {payload} = action;
-    const {data} = yield call(uploadFile, payload);
-    yield put(uploadVideoSuccess(data));
-  } catch (error) {
-    yield put(uploadVideoFailure(error.message));
-  }
-}
+// import {put, takeLatest, call} from 'redux-saga/effects';
+// import axios from 'axios';
+// import {
+//   UPLOAD_VIDEO_REQUEST,
+//   uploadVideoSuccess,
+//   uploadVideoFailure,
+//   setUploadProgress,
+// } from '../actions/UploadActions';
+// import store from '../Store';
+// import api from '../../api/ApiKit';
+// function* uploadVideo(action) {
+//   try {
+//     const {payload} = action;
+//     const {data} = yield call(uploadFile, payload);
+//     yield put(uploadVideoSuccess(data));
+//   } catch (error) {
+//     yield put(uploadVideoFailure(error.message));
+//   }
+// }
 
-function uploadFile(payload) {
-  return new Promise((resolve, reject) => {
-    const config = {
-      onUploadProgress: progressEvent => {
-        const {loaded, total} = progressEvent;
-        const percentCompleted = Math.round((loaded / total) * 100);
-        console.log('Progress:', percentCompleted);
-        store.dispatch(setUploadProgress(percentCompleted));
-      },
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    };
+// function uploadFile(payload) {
+//   return new Promise((resolve, reject) => {
+//     const config = {
+//       onUploadProgress: progressEvent => {
+//         const {loaded, total} = progressEvent;
+//         const percentCompleted = Math.round((loaded / total) * 100);
+//         console.log('Progress:', percentCompleted);
+//         store.dispatch(setUploadProgress(percentCompleted));
+//       },
+//       headers: {
+//         'Content-Type': 'multipart/form-data',
+//       },
+//     };
 
-    axios
-      .post('https://api.escuelajs.co/api/v1/files/upload', payload, config)
-      .then(response => {
-        console.log('Uploaded Successfully');
-        console.log(response.data);
-        resolve(response);
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
-}
+//     axios
+//       .post('https://api.escuelajs.co/api/v1/files/upload', payload, config)
+//       .then(response => {
+//         console.log('Uploaded Successfully');
+//         console.log(response.data);
+//         resolve(response);
+//       })
+//       .catch(error => {
+//         reject(error);
+//       });
+//   });
+// }
 
-export function* watchUploadVideo() {
-  yield takeLatest(UPLOAD_VIDEO_REQUEST, uploadVideo);
-}
+// export function* watchUploadVideo() {
+//   yield takeLatest(UPLOAD_VIDEO_REQUEST, uploadVideo);
+// }
 
 // import {put, takeLatest, call} from 'redux-saga/effects';
 // import axios from 'axios';
@@ -96,3 +97,39 @@ export function* watchUploadVideo() {
 // export function* watchUploadVideo() {
 //   yield takeLatest(UPLOAD_VIDEO_REQUEST, uploadVideo);
 // }
+import {put, takeLatest, call} from 'redux-saga/effects';
+import {
+  UPLOAD_VIDEO_REQUEST,
+  uploadVideoSuccess,
+  uploadVideoFailure,
+  setUploadProgress,
+} from '../actions/UploadActions';
+import {uploadFile} from '../../api/ApiKit';
+import store from '../Store';
+function* uploadVideo(action) {
+  try {
+    const {payload} = action;
+
+    yield put(setUploadProgress(0)); // Reset progress before upload starts
+
+    const {data} = yield call(uploadFile, payload, onUploadProgress);
+
+    console.log('Uploaded Successfully');
+    console.log('Response Data:', data);
+
+    yield put(uploadVideoSuccess(data));
+  } catch (error) {
+    yield put(uploadVideoFailure(error.message));
+  }
+}
+
+function onUploadProgress(progressEvent) {
+  const {loaded, total} = progressEvent;
+  const percentCompleted = Math.round((loaded / total) * 100);
+  console.log('Progress:', percentCompleted);
+  store.dispatch(setUploadProgress(percentCompleted));
+}
+
+export function* watchUploadVideo() {
+  yield takeLatest(UPLOAD_VIDEO_REQUEST, uploadVideo);
+}
