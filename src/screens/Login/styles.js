e04872e2938b3ee8865121a57b0a81f598cb0fd0
Mary Annie <mary.annie2001@gmail.com>
2023-11-21 13:01:21 +0530

    Responsive Design

--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -1,4 +1,8 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet,Dimensions} from 'react-native';
+import {  RFValue } from 'react-native-responsive-fontsize';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
@@ -7,56 +11,63 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'rgba(195, 232, 47, 0.4)',
     paddingBottom: 80,
+    
   },
   container1: {
-    marginRight: 30,
-    marginBottom: 120,
+    width: windowWidth*0.9,
+    marginBottom: windowHeight * 0.14,
+    marginLeft:55,
+    marginTop:10,
   },
   welcomeText: {
-    fontSize: 48,
+    fontSize: RFValue(45),
     color: '#444444',
     fontFamily: 'Poppins-Medium',
   },
   sideText: {
-    fontSize: 19,
+    fontSize: RFValue(18),
     fontFamily: 'Poppins-Medium',
     color: 'grey',
   },
   inputContainer: {
-    width: 320,
-    height: 150,
+    width: windowWidth*0.88,
+    height: windowHeight*0.18,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: windowHeight*0.05,
   },
   password: {
     position: 'absolute',
-    right: 20,
-    top: '33%',
+    // right: "6%",
+    // top: '16%',
+    height:windowHeight*0.117,
+    right:windowWidth*0.06,
   },
   input: {
     color: 'black',
-    paddingLeft: 15,
-    paddingRight: 15,
+    paddingLeft: windowWidth*0.04,
     borderWidth: 2,
     borderRadius: 20,
-    width: '90%',
-    height: 50,
+    width: '87%',
+    height: "38%",
     borderColor: 'grey',
-    paddingBottom: 5,
     fontFamily: 'Poppins-Medium',
-    marginTop: 10,
-  },
+    marginTop: windowHeight*0.013,
+    fontSize: RFValue(13),
+    
+    },
   buttonContainer: {
     backgroundColor: '#444444',
-    width: 180,
+    width: "60%",
+    height:"30%",
     borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 40,
+    marginTop: windowHeight*0.08,
   },
   buttonText: {
     color: '#C3E82F',
-    fontSize: 17,
+    fontSize: RFValue(14),
     fontFamily: 'Poppins-Medium',
   },
   error: {
@@ -65,8 +76,8 @@ const styles = StyleSheet.create({
   wosignin: {
     color: '#444444',
     textDecorationLine: 'underline',
-    fontSize: 16,
-    marginTop: 10,
+    fontSize: RFValue(14),
+    marginTop: windowHeight*0.02,
   },
 });
 export default styles;
